3c68bd6e7f90a534ae34e7a26ed394e2d284b4d2
Bharat Kumar <bharat.0.kumar1234@gmail.com>
2020-07-11 13:42:24 -0400

    TAX-2 adding an interface for files

---
+++ tax/src/file/File.java
@@ -0,0 +1,16 @@
+package file;
+
+public interface File {
+	
+	public void setBinary(Byte[] binary);
+	
+	public Byte[] getBinary();
+	
+	public void setTitle(String title);
+	
+	public String getTitle();
+	
+	public void setDisplayName(String displayName);
+	
+	public String getDisplayName();
+}
